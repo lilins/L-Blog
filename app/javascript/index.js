a59d11190f6021dca63393913eee2b72d6d0a2a3
lilins <lule0102@gmail.com>
2018-12-12 11:28:10 +0800

    add css -loader

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import '../scss/index.scss'
 
 console.log($)
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,12 @@ var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
 //一些文件夹的路径
 var ROOT_PATH = path.resolve(__dirname);
-var SRC_PATH = path.resolve(ROOT_PATH, 'src');
-var MODULE_PATH = path.resolve(ROOT_PATH, './node_modules');
 module.exports = {
   // context webpack处理entry选项时的基础路径
-  context: path.join(__dirname, 'public'),
+  context: path.join(__dirname, 'app'),
   entry: [
     // 会寻找src目录下的index.js
-    '../app/javascript',
+    './javascript',
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
   ],
   output: {
@@ -23,31 +21,28 @@ module.exports = {
       {
         test: /\.scss$/,
         // CSS预处理器们
-        loaders: ['style', 'css', 'postcss', 'sass'],
-        include: SRC_PATH
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ],
+        // loaders: ['postcss'],
+        // include: SCSS_PATH
       }
     ]
   },
   resolve: {
-    // alias: {
-    //   // 为了全局依赖jQuery定义的地址缩写
-    //   jquery: "../node_modules/jquery/dist/jquery.min"
-    // }
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: function () {
-          return [autoprefixer];
-        }
-      }
-    }),
-    // new webpack.ProvidePlugin({
-    //   $: "jquery",
-    //   jQuery: "jquery"
-    // })
+    // new webpack.LoaderOptionsPlugin({
+    //   options: {
+    //     postcss: function () {
+    //       return [autoprefixer];
+    //     }
+    //   }
+    // }),
   ]
 };
